986f5acf97161feffc8c01eeaa4c39dc4b2e6560
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-04-24 10:43:02 +0530

    adding updated files

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// How to create nested html structure using react
-/*
-<div id="parent">
-  <div id="child1">
-    <h1>I am an h1 tag</h1>
-    <h2>I am an h1 tag</h2>
-  </div>
-    <div id="child2">
-    <h1>I am an h1 tag</h1>
-    <h2>I am an h1 tag</h2>
-  </div>
-</div>
-*/
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement('div', { id: parent }, [
   React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'I am an h1 tag'),
+    React.createElement('h1', {}, 'Namaste React by Shubhangi Kumbhars'),
     React.createElement('h2', {}, 'I am an h2 tag'),
   ]),
   React.createElement('div', { id: 'child2' }, [
